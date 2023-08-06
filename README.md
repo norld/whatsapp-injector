77b429c0b4d3c555f1452c0d311b6f4a930b60d7
Made Supardi <grymore@gmail.com>
2023-08-06 23:47:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# whatsapp-injector
+
+this is will be called on script tag for example:
+
+https://cdn.jsdelivr.net/gh/norld/whatsapp-injector/dist/whatsapp.js
---
+++ dist/whatsapp.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("norld-whatsapp");
+
+const imgButton = document.createElement("img");
+imgButton.src = "./whatsapp.png";
+imgButton.id = "whatsapp-button";
+imgButton.style.position = "fixed";
+imgButton.style.bottom = "20px";
+imgButton.style.right = "20px";
+
+container.appendChild(imgButton);
