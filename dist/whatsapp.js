65d06ef1545a5195693ad8b762460161d6a628c2
Made Supardi <grymore@gmail.com>
2023-08-07 00:03:17 +0800

    update example

--- dist/whatsapp.js
+++ dist/whatsapp.js
@@ -1,7 +1,8 @@
-const container = document.getElementById("norld-whatsapp");
-const containerText = document.getElementById("norld-whatsapp-text");
-const textContent = container.textContent;
-const textContentMsg = containerText.textContent;
+const container = document.getElementById("norld-wa");
+const containerNumber = document.getElementById("norld-wa-number");
+const containerText = document.getElementById("norld-wa-text");
+const textContentNumber = containerNumber.textContent;
+const textContentText = containerText.textContent;
 
 const imgButton = document.createElement("img");
 imgButton.src = "https://cdn.jsdelivr.net/gh/norld/whatsapp-injector/dist/whatsapp.png";
@@ -9,12 +10,14 @@ imgButton.id = "whatsapp-button";
 imgButton.style.position = "fixed";
 imgButton.style.bottom = "40px";
 imgButton.style.right = "40px";
+imgButton.style.width = "40px";
+imgButton.style.height = "40px";
 
-container.style.display = "none";
+containerNumber.style.display = "none";
 containerText.style.display = "none";
 
 container.appendChild(imgButton);
 
 imgButton.addEventListener("click", function() {
-  window.location.href = "https://api.whatsapp.com/send?phone="+textContent+"&text="+textContentMsg;
+  window.location.href = "https://api.whatsapp.com/send?phone="+textContentNumber+"&text="+textContentText;
 });
