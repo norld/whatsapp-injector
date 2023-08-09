eefac39d291a2bb1f827ef6c10a7802e2dc34ee5
Made Supardi <grymore@gmail.com>
2023-08-09 12:08:30 +0800

    change to webp image

--- dist/whatsapp.js
+++ dist/whatsapp.js
@@ -33,7 +33,7 @@ flexContainer.addEventListener("mouseleave", function() {
 });
 
 const imgButton = document.createElement("img");
-imgButton.src = "https://cdn.jsdelivr.net/gh/norld/whatsapp-injector/dist/icon.png";
+imgButton.src = "https://cdn.jsdelivr.net/gh/norld/whatsapp-injector@0.1.1/dist/whatsapp.webp";
 imgButton.id = "whatsapp-button";
 imgButton.style.width = "24px";
 imgButton.style.height = "24px";
